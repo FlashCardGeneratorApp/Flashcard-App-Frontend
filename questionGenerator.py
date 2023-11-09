71b5f8947ef766fc5162b21d988252947704d01d
omgitsjohn <syin18@my.bcit.ca>
2023-11-09 14:31:30 -0800

    modified prompt

--- questionGenerator.py
+++ questionGenerator.py
@@ -6,11 +6,11 @@ openai.api_key = config["OPENAI_KEY"]
 
 user_input = input("Text: ")
 prompt = """
-Your response must be a list of dictionary key-value pairing related to the users input. The length of the list of dictionaries is determined by the user. The correct answer is the dictonaries key's value + 1.
+Your response must be a list of dictionary key-value pairing related to the users input. The length of the list of dictionaries is determined by the user. The correct answer is the dictonary key's value.
 If no input is given, then default is 3 questions. 
 
-User: chinese history 1 question
-[{["What dynasty did Qin Shi Huang Found?", "Qing Dynasty", "Han Dynasty", "Song Dynasty", "Zhou Dynasty"]: 1 }]
+User: ww2 2 questions
+[{["When did the second world war begin?", "1939", "1938", "1945", "1944"]: 1 }, {["Who was the leader of Nazi Germany?", "Jospeh Stalin", "Joseph Goebbels", "Heinrich Himmler", "Adolf Hitler"] : 4}]
 User: 
 """
 prompt = f'{prompt}{user_input}\n'
