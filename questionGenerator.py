64f79f0b65456907e6b013a0b08eeaeb740d02e2
omgitsjohn <syin18@my.bcit.ca>
2023-11-22 17:12:42 -0800

    upgraded generate_questions to syntax compatible with gpt4

--- questionGenerator.py
+++ questionGenerator.py
@@ -1,29 +1,20 @@
 import openai
-from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
 
-config = dotenv_values(".env")
-openai.api_key = config["OPENAI_KEY"]
-
+load_dotenv()
+client = openai.OpenAI(api_key=os.getenv("OPENAI_KEY"))
 def generate_questions(user_prompt):
-    prompt = """
-    Your response must be a list of dictionary key-value pairing related to the users input. The length of the list of dictionaries is determined by the user. The correct answer is the dictonary key's value.
-    If no input is given, then default is 3 questions. 
-
-    User: ww2 2 questions
-    [{["When did the second world war begin?", "1939", "1938", "1945", "1944"]: 1 }, {["Who was the leader of Nazi Germany?", "Jospeh Stalin", "Joseph Goebbels", "Heinrich Himmler", "Adolf Hitler"] : 4}]
-    User: 
-    """
-    new_prompt = f'{prompt}{user_prompt}\n'
-
-    response = openai.Completion.create(
+    messages =[{"role": "system","content": """Your response must be a list of dictionary key-value pairing related to the users input. The length of the list of dictionaries is determined by the user. The correct answer is the dictonary key's value.
+    If no input is given, then default is 3 questions."""},
+    {"role": "user","content": "ww2 2 questions"},
+    {"role":"assistant", "content":"""[{["When did the second world war begin?", "1939", "1938", "1945", "1944"]: "1939" }, {["Who was the leader of Nazi Germany?", "Jospeh Stalin", "Joseph Goebbels", "Heinrich Himmler", "Adolf Hitler"] : "Adolf Hitler"}]"""},
+    {"role": "user","content": f"{user_prompt}"},]
+    return client.chat.completions.create(
         model="gpt-4-1106-preview",
-        prompt=new_prompt,
-        # max_tokens=250,
-        stop=".",
+        messages = messages,
         n=1,
-        echo=False,
-    )
-    return response.choices[0].text
+    ).choices[0].message.content
 
 def main():
     user_input = input("Text: ")
