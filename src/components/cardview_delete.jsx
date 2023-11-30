70726656d6059bc31d3ec8210a52aa2e617ebf17
Surafele Asfaw <sasfaw1@my.bcit.ca>
2023-11-29 17:00:54 -0800

    added frontend card view delete page

---
+++ src/components/cardview_delete.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./cardview.css";
+
+const Cardview = () => {
+  const { topic } = useParams();
+  const Question_List = [
+    {
+      question: "What dynasty did Qin Shi Huang Found?",
+      options: ["Qing Dynasty", "Han Dynasty", "Song Dynasty", "Zhou Dynasty"],
+      answer: 1,
+    },
+    {
+      question: "Who orchestrated the Long March?",
+      options: ["Bo Gu", "Mao Ze Dong", "Chiang Kai Shek", "Zhou Enlai"],
+      answer: 2,
+    },
+  ];
+  const [questionList, setQuestionList] = useState([]);
+  const [selectedIndices, setSelectedIndices] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchQuestionList = async () => {
+      try {
+        const response = await fetch(`/api/question/${topic}`); // Change to Django endpoint for AI Generation
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setQuestionList(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching question list:", error);
+        setQuestionList(Question_List); // Setting default data on failure, remove when endpoint exists
+        setIsLoading(false);
+      }
+    };
+
+    fetchQuestionList();
+  }, []);
+
+  const handleClick = (index) => {
+    if (selectedIndices.includes(index)) {
+      setSelectedIndices(selectedIndices.filter((i) => i !== index));
+    } else {
+      setSelectedIndices([...selectedIndices, index]);
+    }
+  };
+
+  const handleClear = () => {
+    setSelectedIndices([]);
+  };
+
+  const handleSubmit = () => {
+    const selectedItems = selectedIndices.map(index => questionList[index]);
+    console.log("Selected Items:", selectedItems);
+    // Implement further submission logic here
+    // TODO Commit and then Redirect
+};
+
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <content>
+      <h2>Topic: {topic}</h2>
+    <div className="card-container">
+      {questionList.map((item, index) => (
+        <div
+          key={index}
+          className={selectedIndices.includes(index) ? "card clicked" : "card"}
+          onClick={() => handleClick(index)}
+        >
+          <h3>{item.question}</h3>
+          <ul className="options-list">
+            {item.options.map((option, optionIndex) => (
+              <li
+                key={optionIndex}
+                className={
+                  optionIndex === item.answer ? "answer-option" : "option"
+                }
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+    <div className="Button-Flex">
+      <button onClick={handleClear}>Clear</button>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+    </content>
+  );
+};
+
+export default Cardview;
